40ce1f5dd23720856b71df423036299b432a523e
kozyilmaz <kazim@monolytic.com>
2017-04-13 13:49:06 +0300

    [macOS] fix serialize ambiguity on Apple platform

--- nheqminer/serialize.h
+++ nheqminer/serialize.h
@@ -654,7 +654,11 @@ void Serialize_impl(Stream& os, const std::vector<T, A>& v, int nType, int nVers
 {
     WriteCompactSize(os, v.size());
     for (typename std::vector<T, A>::const_iterator vi = v.begin(); vi != v.end(); ++vi)
+#if defined(__APPLE__)
+        ::Serialize(os, static_cast<T>(*vi), nType, nVersion);
+#else
         ::Serialize(os, (*vi), nType, nVersion);
+#endif
 }
 
 template<typename Stream, typename T, typename A>
