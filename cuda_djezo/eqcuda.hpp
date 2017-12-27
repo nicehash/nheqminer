16143e2cfacaba86e0f88988bff28ab846eacdd4
Varuna Jayasiri <vpjayasiri@gmail.com>
2017-12-27 10:03:21 +0530

    Fix for CUDA 9.1
    device_functions_decls.h is renamed to device_functions.h

--- cuda_djezo/eqcuda.hpp
+++ cuda_djezo/eqcuda.hpp
@@ -3,7 +3,7 @@
 #include "cuda.h"
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-#include "device_functions_decls.h"
+#include "device_functions.h"
 #include "../cpu_tromp/blake2/blake2.h"
 #include "cuda_djezo.hpp"
 
